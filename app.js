5517f3dd833ba5cdb374358b6511e88096b9b10c
ToniCoder85 <info@iamtoninah.com>
2024-10-18 12:13:30 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("hey you")
+console.log('you made my day')
+let name = 'Tony Stark'
+console.log(name)
+console.log (name)
+name = 'Peppe Potts'
+console. log('name,name')
+
+
+// // const x = 25
+// console. log(x)
+//     x = 30
+//     console. log(x)
+
+ name = 6
+
+console. log(`Hello my name is ${name}`)
